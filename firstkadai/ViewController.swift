aa0dea1f99d30e6601db7eab77cfb9d4df9e5603
akiotan <stead0406@yahoo.co.jp>
2019-04-08 22:49:01 +0900

    Initial Commit

---
+++ firstkadai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstkadai
+//
+//  Created by 大谷明 on 2019/04/08.
+//  Copyright © 2019 大谷明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
